911fc90027c66d49f0bde7764966a781a8180a6a
ionamatell <32112541+Sthaim@users.noreply.github.com>
2021-11-19 15:42:10 +0100

    Début de menu + rotation de caméra

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject text;
+
+    public GameObject sceneJeu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.SetActive(false);
+        /*sceneJeu.SetActive(false);*/
+    }
+
+    // Update is called once per frame
+    void Click()
+    {
+        
+    }
+}
